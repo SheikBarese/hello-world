10054ea151e4e9e5fb255bba897fee0d8d394609
Caio Victor <77304049+caiovictorpcb@users.noreply.github.com>
2021-08-04 12:24:39 -0300

    Update verification.py

--- verification.py
+++ verification.py
@@ -1,31 +1,37 @@
 user = "example23"
 password = "1234"
-option = int
 
-def validate_user(user):
+
+
+def validate_user():
     user_to_validate = input("Digite seu usuário:")
     if user_to_validate != user:
         print("Usuário inválido!")
     else:
-        validate_password(password)
+        validate_password()
 
-def validate_password(password):
+def validate_password():
     password_to_validate = input("Digite sua senha:")
-    if password_to_validate == password and password_to_validate.isnumeric(): #se a senha que escolher contiver letras+números, mudar o "isnumeric" para "isdigit"
+    if password_to_validate == password and password_to_validate.isdigit(): 
         print("Acesso permitido")
     else:
         print("Senha inválida ou incorreta!")
-        stop(option)
+        stop()
 
-def stop(option):
+def stop():
+    print("="*22)
     print("1 - tentar novamente \n2 - desligar o sistema")
-    option = int(input())
-    if option == 1:
-        validate_password(password)
-    elif option == 2:
-        print("Sistema desligado")
+    print("="*22)
+    option = input()
+    if option == "1" or option == "2":
+        if option == "1":
+            validate_password()
+        else:
+            print("Sistema desligado")
     else:
-       print("1 - tentar novamente \n2 - desligar o sistema")
-       stop(option)
+        print("Opção Inválida!")
+        stop()
+        
 
-validate_user(user)
+
+validate_user()
